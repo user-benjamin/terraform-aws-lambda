d8dafd6f779148d3fc1f2ce607f42e47cc7a577c
user-benjamin <benjamin.glover525@gmail.com>
2025-02-28 22:57:21 -0500

    Add required providers

--- versions.tf
+++ versions.tf
@@ -4,10 +4,10 @@ terraform {
   required_version = ">= 1.0.0"
   
   # Example provider requirements
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = ">= 4.0.0, < 5.0.0"
-  #   }
-  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
